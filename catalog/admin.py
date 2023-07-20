8bd88d763ab22c03bd287de4b5e72eb705d79b86
Alexander <Lisnevskiy14@gmail.com>
2023-07-20 23:27:25 +0300

    Correction

--- catalog/admin.py
+++ catalog/admin.py
@@ -5,7 +5,7 @@ from catalog.models import Category, Product
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
-    list_display = ('id', 'name')
+    list_display = ('id', 'name',)
 
 
 @admin.register(Product)
