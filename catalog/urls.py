4c6bd6468169ff92852430b27b2bb85f9e9e0438
Alexander <Lisnevskiy14@gmail.com>
2023-07-22 00:06:52 +0300

    Namespace added

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
 
+from catalog.apps import CatalogConfig
 from catalog.views import home, contacts, product
 
+app_name = CatalogConfig.name
+
 urlpatterns = [
-    path('', home),
-    path('contacts/', contacts),
-    path('product/', product),
+    path('', home, name='home'),
+    path('contacts/', contacts, name='contacts'),
+    path('product/', product, name='product'),
 ]
