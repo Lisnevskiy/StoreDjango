ff6c1c8f1239d0f5d88ddd95b5fc0dfd41fad528
Alexander <Lisnevskiy14@gmail.com>
2023-07-21 23:15:56 +0300

    Added controller product

--- catalog/views.py
+++ catalog/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
 
+from catalog.models import Product
+
 
 def home(request):
-    return render(request, 'catalog/home.html')
+    product_list = Product.objects.all()
+    context = {
+        'object_list': product_list
+    }
+    return render(request, 'catalog/home.html', context)
 
 
 def contacts(request) -> object:
@@ -14,3 +20,11 @@ def contacts(request) -> object:
         message: str = request.POST.get('message')
         print(name, phone, message)
         return render(request, 'catalog/contacts.html')
+
+
+def product(request):
+    product = Product.objects.get(name='Вишня')
+    context = {
+        'product': product
+    }
+    return render(request, 'catalog/product.html', context)
